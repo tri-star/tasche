55cf6606a2fdc772b693599ce7cc0947d19c3a01
tristar <tristar.2nd@gmail.com>
2026-01-26 08:32:34 +0900

    frontendのtestでe2eテストが含まれないように修正

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
     globals: true,
+    exclude: ["**/*.e2e.spec.ts"],
   },
   resolve: {
     alias: {
