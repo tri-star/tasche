eed815288d7e43830a65d2ad1816a2b9fc3a8198
tristar <tristar.2nd@gmail.com>
2026-05-03 22:50:44 +0900

    不要なスタブを利用しないように変更

--- packages/frontend/src/mocks/handlers/index.ts
+++ packages/frontend/src/mocks/handlers/index.ts
@@ -1,6 +1,5 @@
 import type { RequestHandler } from "msw"
 
-import { goalsHandlers } from "./goals"
 import { passthroughHandlers } from "./passthrough"
 
-export const handlers: RequestHandler[] = [...goalsHandlers, ...passthroughHandlers]
+export const handlers: RequestHandler[] = [...passthroughHandlers]
